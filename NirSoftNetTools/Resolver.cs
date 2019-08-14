bc06d8d90fc54b4009d34b64384047e02101e2a0
ilyademin70@gmail.com <ilyademin70@gmail.com>
2019-08-14 22:13:31 +0500

    добавлен базовый функционал

--- NirSoftNetTools/Resolver.cs
+++ NirSoftNetTools/Resolver.cs
@@ -8,8 +8,23 @@ using System.Net;
 
 namespace NirSoftNetTools
 {
-    class Resolver
+    static class Resolver
     {
+        public static List<KeyValuePair<string, IPAddress>> DomainToIP(string hostname)
+        {
+            var list = new List<KeyValuePair<string, IPAddress>>();
+            IPAddress[] IPAddrList = Dns.GetHostAddresses(hostname);
+            foreach (var ip in IPAddrList) 
+                list.Add(new KeyValuePair<string, IPAddress>(hostname, ip));
 
+            return list;
+        }
+
+        public static KeyValuePair<string, IPAddress> IPToDomain(string IP)
+        {
+            IPHostEntry IpToDomainName = Dns.GetHostEntry("173.194.38.138");
+            string hostname = IpToDomainName.HostName;
+            return new KeyValuePair<string, IPAddress>(hostname, IPAddress.Parse(IP));
+        }
     }
 }
