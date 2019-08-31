2563fbf6c87e0100123155754fe6a1addd332ba7
ilyademin70@gmail.com <ilyademin70@gmail.com>
2019-08-31 21:26:49 +0500

    IMEI fix

--- NirSoftNetTools/VendorIMEI.cs
+++ NirSoftNetTools/VendorIMEI.cs
@@ -13,12 +13,9 @@ namespace NirSoftNetTools
         const int IMEILength = 8; 
         public static string[] Lookup(string IMEI)
         {
-            MessageBox.Show("foreach");
             try {
-                MessageBox.Show("foreach");
                 DirectoryInfo dir = new DirectoryInfo(@"data\imei\");
                 foreach (var item in dir.GetFiles()) {
-                    MessageBox.Show("foreach");
                     CSVParser parser = new CSVParser(@"data\imei\" + item.Name);
                     string[] buff;
                     while ((buff = parser.ReadLine()) != null) {
